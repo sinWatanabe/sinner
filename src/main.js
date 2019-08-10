8e3020cb0d34cbae3d88db78bc711f37ab8053b0
sinWatanabe <353205348@qq.com>
2019-08-10 20:25:59 +0800

    modify

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@
 import Vue from "vue"
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+import moment from 'moment'
+Vue.filter('dateFormat',function(datastr,pattren="YYYY-MM-DD HH:mm:ss"){
+    return moment(datastr).format(pattren)
+})
 import VueResource from 'vue-resource'
 Vue.use(VueResource)
+Vue.http.options.root = 'http://www.liulongbin.top:3005';
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 import { Header, Swipe, SwipeItem} from 'mint-ui'
@@ -12,6 +17,7 @@ Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
 import app from './App.vue'
 import router from './router.js'
+import { format } from "util";
 
 var vm = new Vue({
     el: "#app",
